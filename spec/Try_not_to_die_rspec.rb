3b07c043dc53299eeda10a493972d986b0ae78d0
benebess <b3n3b3ss@gmail.com>
2021-05-24 11:01:11 +1000

    created spec file and added an image for winning ascii art

---
+++ spec/Try_not_to_die_rspec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require '/Users/bonepile/Desktop/BenedictePowell_T1A3/SRC/main.rb'
+
+def setup
+    before(:each) do
+      @beginning = Beginning.new
+    end
+end
+
+describe "test" do 
+    
+end
+
+x = yourname
+x.length <= 10
